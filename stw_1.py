6a1221acd3fa5a0056d703031d42f6161468655f
my name <you@example.com>
2017-03-11 17:09:09 +0000

    add script

---
+++ stw_1.py
@@ -0,0 +1,18 @@
+
+# coding: utf-8
+
+# # Save The World V1 
+# 
+# ## Challenge 1
+# 
+# Use data and models available online to compare different energy strategies for UK 
+# 
+# Renuables vs Nuclear vs Focil
+# 
+# 
+# using data and code we should work out how various approaches compare
+
+# In[2]:
+
+print("hello world!")
+
